8aa61c20e7e6a6f125f35bcab6cd520a391268f5
zcs-seu <18351959626@163.com>
2020-02-29 14:57:33 +0800

    Update contains.go
    add index & up contains(3)

--- arrays/contains.go
+++ arrays/contains.go
@@ -1,95 +1,81 @@
 package arrays
 
 import (
-    "reflect"
+	"reflect"
 )
 
-// Contains Returns the index position of the val in array
-func Contains(array interface{}, val interface{}) (index int) {
-    index = -1
-    switch reflect.TypeOf(array).Kind() {
-        case reflect.Slice: {
-            s := reflect.ValueOf(array)
-            for i := 0; i < s.Len(); i++ {
-                if reflect.DeepEqual(val, s.Index(i).Interface()) {
-                    index = i
-                    return
-                }
-            }
-        }
-    }
-    return
+// Contains Returns if or not the val in array
+func Contains(array interface{}, val interface{}) bool {
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		{
+			s := reflect.ValueOf(array)
+			for i := 0; i < s.Len(); i++ {
+				if reflect.DeepEqual(val, s.Index(i).Interface()) {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
 
-// ContainsString Returns the index position of the string val in array
-func ContainsString(array []string, val string) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsString Returns if or not the string val in array
+func ContainsString(array []string, val string) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
-// ContainsInt Returns the index position of the int64 val in array
-func ContainsInt(array []int64, val int64) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsInt Returns if or not the int64 val in array
+func ContainsInt(array []int64, val int64) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
-// ContainsUint Returns the index position of the uint64 val in array
-func ContainsUint(array []uint64, val uint64) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsUint Returns if or not the uint64 val in array
+func ContainsUint(array []uint64, val uint64) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
-// ContainsBool Returns the index position of the bool val in array
-func ContainsBool(array []bool, val bool) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsBool Returns if or not the bool val in array
+func ContainsBool(array []bool, val bool) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
-// ContainsFloat Returns the index position of the float64 val in array
-func ContainsFloat(array []float64, val float64) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsFloat Returns if or not the float64 val in array
+func ContainsFloat(array []float64, val float64) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
 
-// ContainsComplex Returns the index position of the complex128 val in array
-func ContainsComplex(array []complex128, val complex128) (index int) {
-    index = -1
-    for i := 0; i < len(array); i++ {
-        if array[i] == val {
-            index = i
-            return
-        }
-    }
-    return
+// ContainsComplex Returns if or not the complex128 val in array
+func ContainsComplex(array []complex128, val complex128) bool {
+	for i := 0; i < len(array); i++ {
+		if array[i] == val {
+			return true
+		}
+	}
+	return false
 }
-
