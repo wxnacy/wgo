c4928d4caaf5836e8f8fdd34beaf7480703998cb
wxnacy <wxnacy@gmail.com>
2025-10-24 14:28:19 +0800

    feat(pkg/utils): add SerializeToFile

---
+++ pkg/utils/serialize.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"encoding/gob"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+// SerializeToFile 使用 gob 将任意对象编码并写入指定文件路径。
+func SerializeToFile[T any](filePath string, value T) error {
+	if err := os.MkdirAll(filepath.Dir(filePath), 0o755); err != nil {
+		return fmt.Errorf("ensure dir: %w", err)
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("create file: %w", err)
+	}
+	defer file.Close()
+
+	enc := gob.NewEncoder(file)
+	if err := enc.Encode(value); err != nil {
+		return fmt.Errorf("encode gob: %w", err)
+	}
+
+	return nil
+}
+
+// DeserializeFromFile 从文件中读取 gob 数据并反序列化为泛型对象。
+func DeserializeFromFile[T any](filePath string) (T, error) {
+	var result T
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return result, fmt.Errorf("open file: %w", err)
+	}
+	defer file.Close()
+
+	dec := gob.NewDecoder(file)
+	if err := dec.Decode(&result); err != nil {
+		return result, fmt.Errorf("decode gob: %w", err)
+	}
+
+	return result, nil
+}
